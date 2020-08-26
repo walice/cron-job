0f033882333cb8e2e2566f1d143280586509cb35
Alice Lepissier <alice.lepissier@gmail.com>
2020-08-26 07:56:15 +0000

    Last scraped on 2020-08-26 07:56:14.797787

--- execute-notebook.py
+++ execute-notebook.py
@@ -1,4 +1,4 @@
 import papermill as pm
 
-pm.execute_notebook("/home/alice_lepissier/cron-job/Write-Google-Sheets.ipynb",
-                    "/home/alice_lepissier/cron-job/Write-Google-Sheets.ipynb")
+pm.execute_notebook("/home/alice_lepissier/cron-job/index.ipynb",
+                    "/home/alice_lepissier/cron-job/index.ipynb")
